1ebb189a945b1c84c7eead5fb7e877f2cc37de53
bukola-radease <bukola.radease@gmail.com>
2025-10-31 14:58:43 +0100

    range

---
+++ app/components/range.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="text-center my-10 md:my-16">
+         <h2 class="capitalize text-xl md:text-4xl font-bold">Browse the range</h2>
+         <p class="text-sm md:text-base mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+
+</template>
+
+<script setup>
+import dining from "~/assets/images/dining.png"
+import living from "~/assets/images/living.png"
+import bedroom from "~/assets/images/bedroom.png"
+const range = [
+    {img: dining, label: "Dining" },
+    {img: living, label:"Living"},
+    {img: bedroom, label: "Bedroom"}
+]
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,12 +3,14 @@
     <home-hero/>
 
     <div class="max-w-7xl mx-auto">
-
+        <Range/>
     </div>
     </div>
 </template>
 
 <script setup>
+import Range from '~/components/range.vue';
+
 
 definePageMeta({
 layout: "default",
