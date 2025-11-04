c6cf3054e3703776282ebe56ff679356a46e0ead
bukola-radease <bukola.radease@gmail.com>
2025-11-04 16:10:05 +0100

    build error fix

--- app/components/Home/products.vue
+++ app/components/Home/products.vue
@@ -23,14 +23,14 @@
 </template>
 
 <script setup>
-import img1 from "~/assets/images/image 1.png"
-import img2 from "~/assets/images/Image 2.png"
-import img3 from "~/assets/images/Image 3.png"
-import img4 from "~/assets/images/Image 4.png"
-import img5 from "~/assets/images/Image 5.png"
-import img6 from "~/assets/images/Image 6.png"
-import img7 from "~/assets/images/Images 7.png"
-import img8 from "~/assets/images/Image 8.png"
+import img1 from "~/assets/images/img1.png"
+import img2 from "~/assets/images/img2.png"
+import img3 from "~/assets/images/img3.png"
+import img4 from "~/assets/images/img4.png"
+import img5 from "~/assets/images/img5.png"
+import img6 from "~/assets/images/img6.png"
+import img7 from "~/assets/images/img7.png"
+import img8 from "~/assets/images/img8.png"
 
 
 const products = [
